2016c6466634f2a2c61b13b139938171647fa204
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-02 16:53:12 -0700

    Add tests for relationships

--- spec/models/fish_spec.rb
+++ spec/models/fish_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Fish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:aquarium) }
+  end
 end
