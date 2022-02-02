d344df4ccb1f40cef7e181b25c1ec0388f86d7a4
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-01 19:44:42 -0700

    Add before each block for test data

--- spec/features/aquariums/index_spec.rb
+++ spec/features/aquariums/index_spec.rb
@@ -4,18 +4,20 @@ require 'rails_helper'
 # As a visitor
 # When I visit '/parents'
 # Then I see the name of each parent record in the system
-
-shedd = Aquarium.create!(name: 'Shedd Aquarium', clean: true, number_of_types: 100)
-national = Aquarium.create!(name: 'The National Aquarium', clean: true, number_of_types: 200)
-denver = Aquarium.create!(name: 'Denver Aquarium', clean: true, number_of_types: 50)
-
-
 RSpec.describe 'aquarium index' do
+
+before(:each) do
+
+@shedd = Aquarium.create!(name: 'Shedd Aquarium', clean: true, number_of_types: 100)
+@national = Aquarium.create!(name: 'The National Aquarium', clean: true, number_of_types: 200)
+@denver = Aquarium.create!(name: 'Denver Aquarium', clean: true, number_of_types: 50)
+end
+
   scenario 'visitor sees name of each aquarium record in system' do
     visit '/aquariums'
 
-  expect(page).to have_content(shedd.name)
-  expect(page).to have_content(shedd.clean?)
-  expect(page).to have_content(shedd.number_of_types)
+  expect(page).to have_content(@shedd.name)
+  expect(page).to have_content(@national.name)
+  expect(page).to have_content(@denver.name)
   end 
 end
