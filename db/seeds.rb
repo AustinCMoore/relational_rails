ef0301c88d211e87545c4dc95bccd04033c9a9ba
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-01 19:12:20 -0700

    Fix schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+shedd = Aquarium.create!(name: 'Shedd Aquarium', clean: true, number_of_types: 100)
+national = Aquarium.create!(name: 'The National Aquarium', clean: true, number_of_types: 200)
+denver = Aquarium.create!(name: 'Denver Aquarium', clean: true, number_of_types: 50)
+
+baltimore = Dock.create!(name: 'Baltimore Dock', open: true, capacity: 20)
+new_york = Dock.create!(name: 'NYC Dock', open: true, capacity: 30)
+savannah = Dock.create!(name: 'Savannah Dock', open: true, capacity: 10)
