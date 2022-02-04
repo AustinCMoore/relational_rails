eb4690cb6c550db3b4b205fa921d5b62d8246ab5
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-03 19:40:27 -0700

    Add current_path and sad path tests

--- spec/features/aquariums/show_spec.rb
+++ spec/features/aquariums/show_spec.rb
@@ -5,15 +5,16 @@ RSpec.describe 'aquarium show ' do
 before(:each) do
 
 @shedd = Aquarium.create!(name: 'Shedd Aquarium', clean: true, number_of_types: 100)
-@national = Aquarium.create!(name: 'The National Aquarium', clean: true, number_of_types: 200)
 @denver = Aquarium.create!(name: 'Denver Aquarium', clean: true, number_of_types: 50)
 end
 
   scenario 'visitor sees aquarium attributes on specific aquarium show page' do
     visit "/aquariums/#{@shedd.id}"
 
+    expect(current_path).to eq("/aquariums/#{@shedd.id}")
     expect(page).to have_content(@shedd.name)
     expect(page).to have_content(@shedd.clean)
     expect(page).to have_content(@shedd.number_of_types)
+    expect(page).to_not have_content(@denver.name)
   end 
 end 
