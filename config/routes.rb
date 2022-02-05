ad41ccede1a5f2f4b8c02cb16be814df81ebdaa4
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-05 15:27:37 -0700

    Add route for fish index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get '/aquariums', to: 'aquariums#index'
   get 'aquariums/:id', to: 'aquariums#show'
+
+  get '/fish', to: 'fish#index'
+  
   get '/docks', to: 'docks#index'
   get '/docks/:id', to: 'docks#show'
+  get '/docks/:dock_id/boats', to: 'dock_boats#index'
+  
   get '/boats', to: 'boats#index'
   get '/boats/:id', to: 'boats#show'
-  get '/docks/:dock_id/boats', to: 'dock_boats#index'
 end
