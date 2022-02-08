8dab4dd2516702820b1bf6e9e09a6c9e086b867d
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-08 11:20:51 -0700

    Edit boolean validations

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,7 +1,6 @@
 class Fish < ApplicationRecord
   belongs_to :aquarium
   validates_presence_of :name
-  # validates :bioluminescent, inclusion: { 
-  #   in: [true, false] }
+  validates :bioluminescent, inclusion: [true, false]
   validates_presence_of :quantity
 end
