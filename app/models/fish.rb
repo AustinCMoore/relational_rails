abf2a8462183bb5ed7de83a6eba126f74d6789ab
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-07 18:37:49 -0700

    Remove boolean validations

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,7 +1,7 @@
 class Fish < ApplicationRecord
   belongs_to :aquarium
   validates_presence_of :name
-  validates :bioluminescent, inclusion: { 
-    in: [true, false] }
+  # validates :bioluminescent, inclusion: { 
+  #   in: [true, false] }
   validates_presence_of :quantity
 end
--- spec/models/fish_spec.rb
+++ spec/models/fish_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Fish, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :name}
-    it { should validate_presence_of :bioluminescent}
-    it { should allow_value(false).for(:fish) }
+    it { should validate_presence_of :quantity}
+    # it { should validate_presence_of :bioluminescent}
+    # it { should allow_value(false).for(:fish) }
   end
 end
