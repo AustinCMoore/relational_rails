7d472c7fac48ba229cea1a3c3b51380138d84e70
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-07 20:38:57 -0700

    Add new and create methods to controller

--- app/controllers/aquariums_controller.rb
+++ app/controllers/aquariums_controller.rb
@@ -6,4 +6,16 @@ class AquariumsController < ApplicationController
   def show
     @aquarium = Aquarium.find(params[:id])
   end
+
+  def new 
+  end
+
+  def create
+    Aquarium.create(
+      name: params[:name],
+      clean: params[:clean],
+      number_of_types: params[:number_of_types]
+    )
+    redirect_to '/aquariums'
+  end
 end
