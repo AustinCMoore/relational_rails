a8f78eb0fa1211e23ee6c75ff5bbc45e3575ca89
Austin Moore <austinchristianmoore@gmail.com>
2022-02-09 17:39:13 -0500

    Refactor: Clean up repetitive code

--- spec/features/docks/index_spec.rb
+++ spec/features/docks/index_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "docks index page", type: :feature do
     @baltimore = Dock.create!(name: 'Baltimore Dock', open: true, capacity: 20)
     @new_york = Dock.create!(name: 'NYC Dock', open: false, capacity: 30)
     @savannah = Dock.create!(name: 'Savannah Dock', open: true, capacity: 10)
+    visit '/docks'
+
   end
 
   scenario "visitor sees name of each parent record" do
-    visit '/docks'
-
     expect(current_path).to eq('/docks')
     expect(page).to have_content(@baltimore.name)
     expect(page).to have_content(@new_york.name)
@@ -17,8 +17,6 @@ RSpec.describe "docks index page", type: :feature do
   end
 
   scenario "visitor sees records ordered by most recent first with timestamps" do
-    visit '/docks'
-
     within('#dock-0') do
       expect(page).to have_content(@savannah.name)
       expect(page).to have_content(@savannah.created_at)
@@ -36,21 +34,16 @@ RSpec.describe "docks index page", type: :feature do
   end
 
   scenario "visitor clicks link and go to boat index" do
-    visit '/docks'
     click_link('All Boats')
     expect(page).to have_current_path('/boats')
   end
 
   scenario "visitor clicks link and go to dock index" do
-    visit '/docks'
-
     click_link('All Docks')
     expect(page).to have_current_path('/docks')
   end
 
   scenario "visitor clicks link and edits that parents info" do
-    visit '/docks'
-
     click_link("Edit #{@baltimore.name}")
     expect(page).to have_current_path("/docks/#{@baltimore.id}/edit")
   end
