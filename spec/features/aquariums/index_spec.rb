182a093261130296dae005c1d980cab01dccc938
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-08 20:30:06 -0700

    Clean up aquarium index spec

--- spec/features/aquariums/index_spec.rb
+++ spec/features/aquariums/index_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe 'aquarium index' do
 
 before(:each) do
+  @shedd = Aquarium.create!(name: 'Shedd Aquarium', clean: true, number_of_types: 100)
+  @denver = Aquarium.create!(name: 'Denver Aquarium', clean: true, number_of_types: 50)
+  @nyc = Aquarium.create!(name: 'New York Aquarium', clean: true, number_of_types: 150)
 
-@shedd = Aquarium.create!(name: 'Shedd Aquarium', clean: true, number_of_types: 100)
-@denver = Aquarium.create!(name: 'Denver Aquarium', clean: true, number_of_types: 50)
-@nyc = Aquarium.create!(name: 'New York Aquarium', clean: true, number_of_types: 150)
+  visit "/aquariums/"
 end
 
   scenario 'visitor sees name of each aquarium record in system' do
-    visit '/aquariums'
 
-    expect(current_path).to eq("/aquariums")
+    expect(current_path).to eq("/aquariums/")
     expect(page).to have_content(@shedd.name)
     expect(page).to have_content(@nyc.name)
     expect(page).to have_content(@denver.name)
@@ -21,26 +21,22 @@ end
   end
 
    scenario 'visitor sees link for fish index' do
-    visit "/aquariums/"
-
     click_link("View Fish")
    expect(current_path).to eq( "/fish")
 
   end
 
-  scenario 'visitor sees link for aquarium index' do
-    visit "/aquariums"
+    scenario 'visitor sees link for aquarium index' do
 
    click_link("View Aquariums")
    expect(current_path).to eq( "/aquariums")
   end
 
   scenario 'visitor sees records ordered by #created_at and timestamp' do
-    visit '/aquariums'
 
     expect(page).to have_content(@shedd.created_at)
     expect(page).to have_content(@nyc.created_at)
     expect(page).to have_content(@denver.created_at)
     expect(@denver.name).to appear_before(@shedd.name)
   end
-end
+end 
