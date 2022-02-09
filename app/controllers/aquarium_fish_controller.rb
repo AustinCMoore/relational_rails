51ddc0f1b76551d90629e3d9771c1592031a0ad0
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-08 18:56:04 -0700

    Add new and create methods

--- app/controllers/aquarium_fish_controller.rb
+++ app/controllers/aquarium_fish_controller.rb
@@ -3,4 +3,18 @@ class AquariumFishController < ApplicationController
     @aquarium = Aquarium.find(params[:aquarium_id])
     @fishies = @aquarium.fish
   end
+
+  def new
+    @aquarium = Aquarium.find(params[:aquarium_id])
+  end
+
+  def create
+    @aquarium = Aquarium.find(params[:aquarium_id])
+    @fish = @aquarium.fish.create(
+      name: params[:name],
+      quantity: params[:quantity],
+      bioluminescent: params[:bioluminescent]
+    )
+    redirect_to "/aquariums/#{@aquarium.id}/fish"
+  end
 end 
