e0b726e23ec5a41aca8095c48329db798f7821c4
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-09 14:51:43 -0700

    Add boat model test

--- spec/models/boat_spec.rb
+++ spec/models/boat_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe Boat, type: :model do
     it { should allow_value(false).for(:motor_powered)}
     it { should allow_value(true).for(:motor_powered)}
   end
+
+  describe 'class methods' do
+
+    before(:each) do
+      @baltimore = Dock.create!(name: 'Baltimore Dock', open: true, capacity: 20)
+      @hobie = @baltimore.boats.create!(name: 'Hobie', motor_powered: false, crew_size: 2)
+      @sea_ray = @baltimore.boats.create!(name: 'Sea Ray', motor_powered: true, crew_size: 3)
+    end
+
+    it '#self.is_motor_powered' do
+      expect(Boat.is_motor_powered).to eq([@sea_ray])
+    end
+  end
 end
